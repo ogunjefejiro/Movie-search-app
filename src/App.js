320690d96260693bcd0c9adf2dbd2f76c9b7faf8
Fejiro Ogunje <ogunjefejiro@gmail.com>
2022-02-24 09:54:36 +0100

    added spinner

--- src/App.js
+++ src/App.js
@@ -6,57 +6,67 @@ import Results from "./components/Results";
 import Searchbar from "./components/Searchbar";
 import Popup from "./components/Popup";
 
-
 function App() {
-
   const [state, setState] = useState({
     s: "",
     results: [],
-    selected: {}
-  })
+    selected: {},
+  });
+  const [loading, setLoading] = useState(false);
 
-  const apiurl = "https://www.omdbapi.com/?apikey=4533d35f"
+  const apiurl = "https://www.omdbapi.com/?apikey=4533d35f";
 
   const handleInput = (e) => {
-    let s = e.target.value
-    setState({...state, s: s})
-  }
+    let s = e.target.value;
+    setState({ ...state, s: s });
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setLoading(true);
 
-//add if statement to prevent it from submitting if the form is empty
+    //add if statement to prevent it from submitting if the form is empty
 
     axios(`${apiurl}&s=${state.s}`)
-      .then(({data}) => {
-        let results = data.Search
-        console.log(results)
-        setState({...state, results: results})
+      .then(({ data }) => {
+        setLoading(false);
+        let results = data.Search;
+        console.log(results);
+        setState({ ...state, results: results });
       })
-      .catch(err => console.log(err))
-
-  } 
+      .catch((err) => {
+        setLoading(false);
+        console.log(err);
+      });
+  };
 
   const openPopup = (id) => {
-    axios(`${apiurl}&i=${id}`)
-      .then(({data}) => {
-        let result = data;
-        console.log(result)
-        setState({...state, selected: result})
-      })
-      console.log(state.selected)
-  }
-  
+    axios(`${apiurl}&i=${id}`).then(({ data }) => {
+      let result = data;
+      console.log(result);
+      setState({ ...state, selected: result });
+    });
+    console.log(state.selected);
+  };
+
   const closePopup = () => {
-    setState({...state, selected: {}})
-  }
+    setState({ ...state, selected: {} });
+  };
 
   return (
     <div className="container">
-      <Header/>
-      <Searchbar handleSubmit={handleSubmit} handleInput={handleInput}/>
-      <Results results={state.results} openPopup={openPopup}/>
-      {(typeof state.selected.Title != "undefined") ? <Popup selected={state.selected} closePopup={closePopup}/> : false}
+      <Header />
+      <Searchbar handleSubmit={handleSubmit} handleInput={handleInput} />
+      <Results
+        results={state.results}
+        openPopup={openPopup}
+        loading={loading}
+      />
+      {typeof state.selected.Title != "undefined" ? (
+        <Popup selected={state.selected} closePopup={closePopup} />
+      ) : (
+        false
+      )}
     </div>
   );
 }
--- src/components/Results.js
+++ src/components/Results.js
@@ -1,12 +1,22 @@
-import React from 'react'
-import Result from './Result'
+import React from "react";
+import Result from "./Result";
 
-const Results = ({results, openPopup}) => {
-    return (
-        <div className="results-container">
-            {results.map(result => (<Result key={result.imdbID} result={result} openPopup={openPopup}/>))}
+const Results = ({ results, openPopup, loading }) => {
+  return (
+    <div>
+      {loading ? (
+        <div className="loading">
+          <img src="/spinner.gif" alt="" className="spinner" />
         </div>
-    )
-}
+      ) : (
+        <div className="results-container">
+          {results.map((result) => (
+            <Result key={result.imdbID} result={result} openPopup={openPopup} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
 
-export default Results
+export default Results;
